071b00f757453f526aa60262e4368431730468c5
root <jeikelschez@hotmail.com>
2018-04-20 01:05:33 -0400

    Actualizacion Jeikel Sanchez 19-04-2018 Correccion cargar ruta duplicada

--- app/show_routes/route_upload_add_results.php
+++ app/show_routes/route_upload_add_results.php
@@ -10,29 +10,57 @@ if ($conn->connect_error) {
 	$date_route = $_POST['date_route'];
     $fday = $_POST['presentation_date0'];
 
-$sql1 = "SELECT det.PRESENTATION_DATE as FIRST_DATE
+$sql1 = "SELECT ro.ROUTESID as ROUTESID
          FROM routes ro, routes_det det 
          WHERE ro.ROUTESID = det.ROUTESID
          AND ro.SHOWID = $showid
-         ORDER BY det.PRESENTATION_DATE
-         LIMIT 1";  
+         AND det.PRESENTATION_DATE = '$fday'";
 
 $result = $conn->query($sql1);
 while ($row = $result->fetch_assoc()){
-    $fday_orig = $row['FIRST_DATE'];
-}   
+    $id = $row['ROUTESID'];
+}          
 
-if(isset($fday_orig)){
-    if($fday==$fday_orig){
-        echo "The route is already loaded for this show on this date";
-        exit();
-    }
-}   
+if(isset($id)){
 
-$sql2 = "INSERT INTO routes (SHOWID,TRUCKS,DATE_OF_ROUTE,WEEKLY_NUT)
+    $sql2 = "SELECT det.PRESENTATION_DATE as FIRST_DATE
+             FROM routes ro, routes_det det 
+             WHERE ro.ROUTESID = det.ROUTESID
+             AND ro.ROUTESID = $id
+             ORDER BY det.PRESENTATION_DATE
+             LIMIT 1";  
+
+    $result = $conn->query($sql2);
+    while ($row = $result->fetch_assoc()){
+        $fday_orig = $row['FIRST_DATE'];
+    }   
+
+    if(isset($fday_orig)){
+        if($fday==$fday_orig){
+            echo "The route is already loaded for this show on this date";
+
+            echo "
+            <script language=\"javascript\"
+                type=\"text/javascript\">
+                function windowClose() {
+                    window.open('','_parent','');
+                    window.opener.location.reload();
+                    window.close();
+                }
+            </script>
+            <p align=center>
+            <input type=\"button\" value=\"CLOSE THIS WINDOW\" onclick=\"windowClose();\"></p>";
+            $conn->close();
+            include '../footer.html';
+            exit();
+        }
+    } 
+}      
+
+$sql3 = "INSERT INTO routes (SHOWID,TRUCKS,DATE_OF_ROUTE,WEEKLY_NUT)
 				VALUES ($showid, $numberoftrucks, '$date_route', $weeklynut)";
 
-if($conn->query($sql2) === TRUE) {
+if($conn->query($sql3) === TRUE) {
 
     $routeid = $conn->insert_id;
 
@@ -78,10 +106,10 @@ if($conn->query($sql2) === TRUE) {
     	if(empty($_POST["deal_memo" . $i])){$deal_memo = 0;}else{$deal_memo = 1;} 
     	if(empty($_POST["contract" . $i])){$contract = 0;}else{$contract = 1;}     	
 
-    	$sql3 = "INSERT INTO routes_det (ROUTESID,PRESENTATION_DATE,HOLIDAY,CITYID,`REPEAT`,MILEAGE,BOOK_NOTES,PROD_NOTES,TIME_ZONE,SHOW_TIMES,PERF,VENUEID,PRESENTERID,CAPACITY,FIXED_GNTEE,ROYALTY,BACKEND,BREAKEVEN,DEAL_NOTES,EST_ROYALTY,ON_SUB,DATE_CONF,OFFER,PRICE_SCALES,EXPENSES,DEAL_MEMO,CONTRACT)
+    	$sql4 = "INSERT INTO routes_det (ROUTESID,PRESENTATION_DATE,HOLIDAY,CITYID,`REPEAT`,MILEAGE,BOOK_NOTES,PROD_NOTES,TIME_ZONE,SHOW_TIMES,PERF,VENUEID,PRESENTERID,CAPACITY,FIXED_GNTEE,ROYALTY,BACKEND,BREAKEVEN,DEAL_NOTES,EST_ROYALTY,ON_SUB,DATE_CONF,OFFER,PRICE_SCALES,EXPENSES,DEAL_MEMO,CONTRACT)
 				VALUES ($routeid,'$presentation_date',$holiday,$city,$repeat,$mileage,'$book_notes','$prod_notes','$time_zone','$show_times',$perf,$venue,$presenter,$capacity,$fixed_gntee,$royalty,$backend,$breakeven,'$deal_notes',$est_royalty,$on_sub,$date_conf,$offer,$price_scales,$expenses,$deal_memo,$contract)";
 
-		if ($conn->query($sql3) === TRUE) {
+		if ($conn->query($sql4) === TRUE) {
 			if(($i+1) ==364){
 				echo "Record Created successfully";
 			}
