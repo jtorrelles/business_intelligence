44769ae3693d21ba85a222536dd0ec9943994210
RGConnectIT <38170908+RGConnectIT@users.noreply.github.com>
2018-05-01 13:28:44 -0600

    Cambios por seguridad
    Eliminados los nombres de la BD, servidor, usuario y password por razones de seguridad

--- app/db/database_conn.php
+++ app/db/database_conn.php
@@ -1,11 +1,7 @@
 <?php
-/*$servername = "localhost";
-$username = "bi_networks";
-$password = "NETworks2018!";
-$dbname = "networksbi";*/
-$servername = "userverdev";
-$username = "root";
-$password = "1234";
-$dbname = "networksbi";
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
 $conn = new mysqli($servername, $username, $password, $dbname);
 ?>
