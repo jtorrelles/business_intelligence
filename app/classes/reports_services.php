80f21dff760308009241b815963beefac2fe9a1c
jtorrelles <jhonnattan.torrelles@gmail.com>
2018-05-06 21:07:08 -0400

    update file report_services

--- app/classes/reports_services.php
+++ app/classes/reports_services.php
@@ -28,13 +28,63 @@ class reportsServices extends reports_basics {
       $res = reports_basics::getAllShows();
 
       if(!$res) {
-      	throw new exception("Data not found.");
+        throw new exception("Data not found.");
       }
 
       //De la respuesta que es un array, en este caso solo se necesito el atributo 'result'
       $data = array('status'=>'success', 'tp'=>1, 'msg'=>"Data fetched successfully.", 'result'=>$res['result']);
 
     } catch (Exception $e) {
+      $data = array('status'=>'error', 'tp'=>0, 'msg'=>$e->getMessage());
+    } finally {
+      return $data;
+    }
+  }
+
+  //Función para Reporte AllRoutes
+  public static function getAllRoutes($finicio, $ffin){
+    try{
+
+      $res = array();
+
+      //Ejecución del Metodo getAllShows de la Clase Padre (basica)
+      $res = reports_basics::getAllShows();
+      $shows = $res['result']['shows'];
+
+      $UTC = new DateTimeZone("UTC"); 
+      $ini = new DateTime($finicio, $UTC); 
+      $end = new DateTime($ffin, $UTC); 
+
+      $initdate = $ini->format('Ymd');
+      $enddate = $end->format('Ymd');
+
+      foreach ($shows as $show => $attrib){
+          $showid = $attrib->id;
+          $showname = $attrib->name;
+
+          $query2 = "SELECT IFNULL((SELECT CONCAT(ci.name,'/',sta.name)
+                               FROM cities ci, 
+                                    states sta
+                               WHERE ci.id = det.cityid
+                                 AND ci.state_id = sta.id),'') as citystate 
+                       FROM shows sh, routes ro, routes_det det 
+                      WHERE sh.showid = ro.showid 
+                        AND ro.routesid = det.routesid 
+                        AND sh.showid = 4 
+                        AND det.presentation_date BETWEEN '$initdate' AND '$enddate'";
+
+          $result2 = reports_basics::run($query2);
+          /*$result2 = reports_basics::run2($query2);*/
+          if(!$result2) {
+            throw new exception("City/State not found.");
+          }
+
+          //$json = mysqli_fetch_all ($result2, MYSQLI_ASSOC);
+
+        }
+
+      $data = array('status'=>'success', 'tp'=>1, 'msg'=>"Data fetched successfully.", 'result'=>$shows);
+    }catch (Exception $e) {
       $data = array('status'=>'error', 'tp'=>0, 'msg'=>$e->getMessage());
     } finally {
       return $data;
