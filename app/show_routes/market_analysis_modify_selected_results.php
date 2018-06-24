06e56363df42913ce4338f181bf8e71b3932d31a
Ramiro Gonzalez <38170908+RGConnectIT@users.noreply.github.com>
2018-06-24 17:09:33 -0600

    Nuevo Header 6/24/18

--- app/show_routes/market_analysis_modify_selected_results.php
+++ app/show_routes/market_analysis_modify_selected_results.php
@@ -2,7 +2,7 @@
 require '../db/database_conn.php';
 include '../session.php';
 include 'access_control.php';
-include '../header.html';
+include '../header_nologout.html';
 
 include '../security_log.php';
 
