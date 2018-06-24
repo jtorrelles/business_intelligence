b9aa045337d5d46d4a32e635e66557f2f7ed36aa
Ramiro Gonzalez <38170908+RGConnectIT@users.noreply.github.com>
2018-06-24 17:11:47 -0600

    Nuevo Header 6/24/18

--- app/contracts/contract_delete_selected_results.php
+++ app/contracts/contract_delete_selected_results.php
@@ -2,7 +2,7 @@
 require '../db/database_conn.php';
 include '../session.php';
 include 'access_control.php';
-include '../header.html';
+include '../header_nologout.html';
 $description = "Deleted Approved Deals & Terms ID: ".$_POST['id_contract'];
 include '../security_log.php';
 if ($conn->connect_error) {
