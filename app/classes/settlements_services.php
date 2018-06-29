7ed9de977c53f43c56f62a1b719f04335dca9d37
Ramiro Gonzalez <38170908+RGConnectIT@users.noreply.github.com>
2018-06-29 13:42:20 -0600

    Otra mejora a getCities() 6/29/18

--- app/classes/settlements_services.php
+++ app/classes/settlements_services.php
@@ -160,7 +160,7 @@ class settlementsServices extends dbconfig {
   public static function getCities($stateId) {
      try {
 
-      $query = "SELECT id, name FROM cities WHERE id IN (select venues.VenueCITYID from venues GROUP BY venues.VenueCITYID) AND state_id= $stateId ORDER BY name ASC";
+      $query = "SELECT id, name FROM cities WHERE id IN (select venues.VenueCITYID from venues WHERE venues.VenueACTIVE = 'Y' GROUP BY venues.VenueCITYID) AND state_id= $stateId ORDER BY name ASC";
       $result = dbconfig::run($query);
       if(!$result) {
        throw new exception("City not found.");
