045fd4c3e7f6c07eb3eb8fec925795722a5f86f6
Ramiro Gonzalez <38170908+RGConnectIT@users.noreply.github.com>
2018-06-29 06:48:36 -0600

    Mejora en getCities() 6/29/18
    La mejora en getCities toma la id de las ciudades que tienen asignadas directamente de la tabla venues y no de settlements. Esto se acomoda con el ideal de utilizar venues como el punto de control para la ubicacion de cualquier accion.

--- app/classes/settlements_services.php
+++ app/classes/settlements_services.php
@@ -138,7 +138,7 @@ class settlementsServices extends dbconfig {
     // Fetch all cities list by state id
   public static function getCities($stateId) {
      try {
-       $query = "SELECT id, name FROM cities WHERE id IN (select settlements.CITYID from settlements GROUP BY settlements.CITYID) AND state_id=".$stateId;
+       $query = "SELECT id, name FROM cities WHERE id IN (select venues.VenueCITYID from venues GROUP BY venues.VenueCITYID) AND state_id=".$stateId;
        $result = dbconfig::run($query);
        if(!$result) {
          throw new exception("City not found.");
