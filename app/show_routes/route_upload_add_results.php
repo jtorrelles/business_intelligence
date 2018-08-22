c2dae77498cee778f8f42a55eb0c1c24888c8657
Ramiro Gonzalez <38170908+RGConnectIT@users.noreply.github.com>
2018-08-22 16:48:27 -0600

    Error corregido 8/22/2018
    Se corrige error que no permite subir archivos que tengan comillas y espacios en blanco en la hoja de excel.

--- app/show_routes/route_upload_add_results.php
+++ app/show_routes/route_upload_add_results.php
@@ -42,7 +42,7 @@ if($fday<=$maxdate){
 }      
 
 $sql3 = "INSERT INTO routes (SHOWID,TRUCKS,DATE_OF_ROUTE,WEEKLY_NUT,XLSX_FILENAME)
-				VALUES ($showid, $numberoftrucks, '$date_route', $weeklynut, \"$xlsx_filename\")";
+				VALUES ($showid, $numberoftrucks, \"$date_route\", $weeklynut, \"$xlsx_filename\")";
 
 if($conn->query($sql3) === TRUE) {
 
@@ -57,11 +57,11 @@ if($conn->query($sql3) === TRUE) {
     	if(empty($_POST["repeat" . $i])){$repeat = 0;}else{$repeat = 1;} 
     	$mileage = $_POST["mileage" . $i];	
     	if(empty($mileage)){$mileage = 0.00;}else{$mileage = $mileage;} 
-    	$book_notes = $_POST["book_notes" . $i];
-    	$prod_notes = $_POST["prod_notes" . $i];
+    	$book_notes = str_replace("\"","''",$_POST["book_notes" . $i]);
+    	$prod_notes = str_replace("\"","''",$_POST["prod_notes" . $i]);
     	$time_zone = $_POST["time_zone" . $i];
     	$show_times = $_POST["show_times" . $i];
-    	$perf = $_POST["perf" . $i];	
+    	$perf = $_POST["perf" . $i];		
     	if(empty($perf)){$perf = 0;}else{$perf = $perf;}
     	$venue = substr($_POST["venue" . $i],strpos($_POST["venue" . $i],"//")+2);
     	if(empty($venue)){$venue = "NULL";}else{$venue = $venue;} 
@@ -80,7 +80,7 @@ if($conn->query($sql3) === TRUE) {
     	$breakeven = str_replace("%","",$_POST["breakeven" . $i]);	
     	if(strpos($breakeven,'.')==true){$breakeven = $breakeven * 100;}else{$breakeven = str_replace(",",".",$breakeven);}
     	if(empty($breakeven)){$breakeven = 0.00;}else{$breakeven = $breakeven;}
-    	$deal_notes = $_POST["deal_notes" . $i];
+    	$deal_notes = str_replace("\"","''",$_POST["deal_notes" . $i]);
     	$est_royalty = str_replace(",",".",$_POST["est_royalty" . $i]);	
     	if(empty($est_royalty)){$est_royalty = 0.00;}else{$est_royalty = $est_royalty;}
     	if(empty($_POST["on_sub" . $i])){$on_sub = 0;}else{$on_sub = 1;} 
@@ -89,10 +89,9 @@ if($conn->query($sql3) === TRUE) {
     	if(empty($_POST["price_scales" . $i])){$price_scales = 0;}else{$price_scales = 1;} 
     	if(empty($_POST["expenses" . $i])){$expenses = 0;}else{$expenses = 1;} 
     	if(empty($_POST["deal_memo" . $i])){$deal_memo = 0;}else{$deal_memo = 1;} 
-    	if(empty($_POST["contract" . $i])){$contract = 0;}else{$contract = 1;}     	
-
+    	if(empty($_POST["contract" . $i])){$contract = 0;}else{$contract = 1;}
     	$sql4 = "INSERT INTO routes_det (ROUTESID,PRESENTATION_DATE,HOLIDAY,CITYID,`REPEAT`,MILEAGE,BOOK_NOTES,PROD_NOTES,TIME_ZONE,SHOW_TIMES,PERF,VENUEID,PRESENTERID,CAPACITY,FIXED_GNTEE,ROYALTY,BACKEND,BREAKEVEN,DEAL_NOTES,EST_ROYALTY,ON_SUB,DATE_CONF,OFFER,PRICE_SCALES,EXPENSES,DEAL_MEMO,CONTRACT)
-				VALUES ($routeid,'$presentation_date',$holiday,$city,$repeat,$mileage,'$book_notes','$prod_notes','$time_zone','$show_times',$perf,$venue,$presenter,$capacity,$fixed_gntee,$royalty,$backend,$breakeven,'$deal_notes',$est_royalty,$on_sub,$date_conf,$offer,$price_scales,$expenses,$deal_memo,$contract)";
+				VALUES ($routeid,\"$presentation_date\",$holiday,$city,$repeat,$mileage,\"$book_notes\",\"$prod_notes\",\"$time_zone\",\"$show_times\",$perf,$venue,$presenter,$capacity,$fixed_gntee,$royalty,$backend,$breakeven,\"$deal_notes\",$est_royalty,$on_sub,$date_conf,$offer,$price_scales,$expenses,$deal_memo,$contract)";
 
 		if ($conn->query($sql4) === TRUE) {
 			if(($i+1) ==364){
